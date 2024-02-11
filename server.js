1e598f5b046bbd3fb3ae1c96f9ca1b394424c920
sanjayryker <sanjaypandian01@gmail.com>
2024-02-11 13:50:34 +0530

    Server updated

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 4000
 //Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors())
+app.use(cors({
+    origin : ["http://localhost:3000","https://workout-tracker-m409.onrender.com"]
+}));
 // routes
 app.use('/api/workout',workoutRoute)
 app.use('/api/user',userRoutes)
